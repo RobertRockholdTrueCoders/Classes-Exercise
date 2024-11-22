ff8150ba3affdfc73c36e99eb11bb07f93de5cce
Robert <broncofan3124@msn.com>
2024-11-21 16:38:52 -0800

    Completed

--- Classes/Car.cs
+++ Classes/Car.cs
@@ -1,6 +1,11 @@
+using System.Threading.Channels;
+
 namespace Classes;
 
 public class Car
 {
-    
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+        
 }
